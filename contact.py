7437014e88a7e9e29a7b5b518960eb4d1dcfa5bb
ssomkov <ssomkov@gmail.com>
2018-12-02 18:04:56 +0400

    Add Contact class

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nick_name, title, company, first_address, home_phone,
+                 mobile_phone, work_phone, fax, email, email2, email3, homepage, birthday_day, birthday_month,
+                 birthday_year, anniversary_day, anniversary_month, anniversary_year, group, secondary_address,
+                 secondary_home, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nick_name = nick_name
+        self.title = title
+        self.company = company
+        self.first_address = first_address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.birthday_day = birthday_day
+        self.birthday_month = birthday_month
+        self.birthday_year = birthday_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
+        self.group = group
+        self.secondary_address = secondary_address
+        self.secondary_home = secondary_home
+        self.notes = notes
