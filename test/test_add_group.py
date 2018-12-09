5f7eb289f22924eeb36bf332655c785de7e42a9f
ssomkov <ssomkov@gmail.com>
2018-12-09 15:14:35 +0400

    Add GroupHelper

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -14,5 +14,5 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(user_name="admin", user_password="secret")
-    app.create_group(Group(name="test group", footer="logo of group", header="comment for group"))
+    app.group.create(Group(name="test group", footer="logo of group", header="comment for group"))
     app.session.logout()
