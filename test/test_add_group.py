4a78295be9890b47b424481c4cdf42b1d15d1ef9
ssomkov <ssomkov@gmail.com>
2018-12-22 21:56:53 +0400

    Fix test_add_group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+
 from model.group import Group
 
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="test group", footer="logo of group", header="comment for group"))
+    group = Group(name="test group", footer="logo of group", header="comment for group")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
