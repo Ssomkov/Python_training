02fb0c6d7fcdd0968c6454124a11528973b2063a
ssomkov <ssomkov@gmail.com>
2018-12-30 23:01:58 +0400

    Add work with parametres in test_add_group

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,11 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import random
+import string
+
+import pytest
+
 from model.group import Group
 
 
-def test_add_group(app):
+def get_random_string(prefix, max_length):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(max_length))])
+
+
+test_data = [Group(name=get_random_string("group name", 10), footer=get_random_string("group footer", 10),
+                   header=get_random_string("group header", 10)),
+             Group(name=get_random_string("group name", 10), footer=get_random_string("group footer", 10),
+                   header=get_random_string("group header", 10))]
+
+
+def get_ids():
+    return [repr(x) for x in test_data]
+
+
+@pytest.mark.parametrize("group", test_data, ids=get_ids())
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="test group", footer="logo of group", header="comment for group")
     app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
