0056fa7979a0ec1b8659a7d1ca0e1ea04e776949
ssomkov <ssomkov@gmail.com>
2019-01-31 16:32:36 +0400

    add length assertion

--- test/test_contacts_info.py
+++ test/test_contacts_info.py
@@ -7,6 +7,8 @@ def test_contacts_info_on_home_page(app, db):
     contacts_from_bd = sorted(db.get_contact_list(), key=Contact.id_or_max)
     contacts_from_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
+    assert len(contacts_from_bd) == len(contacts_from_page)
+
     for item in range(len(contacts_from_bd)):
         assert contacts_from_bd[item].first_name == contacts_from_page[item].first_name
         assert contacts_from_bd[item].last_name == contacts_from_page[item].last_name
