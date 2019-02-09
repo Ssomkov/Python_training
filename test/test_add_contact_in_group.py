9bd463a84e891c6b54407dd51764cd487ef94390
ssomkov <ssomkov@gmail.com>
2019-02-09 20:05:10 +0400

    fix add contact in group

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -9,30 +9,19 @@ def test_add_contact_in_group(app, db, db_orm):
     if app.contact.count() == 0:
         app.contact.create(
             Contact(first_name="created_first_name", middle_name="created_middle_name", last_name="created_last_name"))
-    # список контактов из БД
-    old_contacts = db.get_contact_list()
-    # берем рандомный контакт
-    contact = random.choice(old_contacts)
-    contact_id = contact.id
-    # создаем новую группу
-    app.group.create(
-        Group(name="created group", footer="created logo of group", header="created comment for group"))
+    # создаем группу если групп нет
+    if len(db.get_group_list()) == 0:
+        app.group.create(
+            Group(name="created group", footer="created logo of group", header="created comment for group"))
     # список групп из БД
     old_groups = db.get_group_list()
-    is_contact_in_group = True
-
-    # выбираем группу в которой нет добавляемого контакта
-    while is_contact_in_group == True:
-        # берем рандомную группу
-        group = random.choice(old_groups)
-        group_id = group.id
-        # проверяем что в этой группе нет данного контакта
-        contacts_in_group = db_orm.get_contacts_in_group(group)
-        for item in range(len(contacts_in_group)):
-            if contacts_in_group[item].id == contact_id:
-                is_contact_in_group = False
-                break
-
+    # берем рандомную группу
+    group = random.choice(old_groups)
+    group_id = group.id
+    # берем контакты не входящие в эту группу
+    contacts_not_in_group = db_orm.get_contacts_not_in_group(group)
+    contact = random.choice(contacts_not_in_group)
+    contact_id = contact.id
     # добавляем контакт в группу
     app.contact.add_to_group_by_id(contact_id, group_id)
     is_contact_in_group = False
