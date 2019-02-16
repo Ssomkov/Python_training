72aed7d98793f85faf8bd7077a033ca938a3fa39
ssomkov <ssomkov@gmail.com>
2019-02-16 15:51:00 +0400

    Add gherkin for groups

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,11 @@
+from pytest_bdd import scenario
+
+
+@scenario('groups.feature', 'Add new group')
+def test_add_new_group():
+    pass
+
+
+@scenario('groups.feature', 'Delete a group')
+def test_delete_group():
+    pass
