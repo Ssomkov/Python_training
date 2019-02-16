89aff126d54f8643a142e35049974e19f0ec92a1
ssomkov <ssomkov@gmail.com>
2019-02-16 20:11:43 +0400

    add allure steps

--- test/test_add_contact_in_group.py
+++ test/test_add_contact_in_group.py
@@ -1,34 +1,43 @@
 import random
 
+import pytest
+
 from model.contact import Contact
 from model.group import Group
 
 
 def test_add_contact_in_group(app, db, db_orm):
     # создаем новый контакт если контактов нет
-    if app.contact.count() == 0:
-        app.contact.create(
-            Contact(first_name="created_first_name", middle_name="created_middle_name", last_name="created_last_name"))
+    with pytest.allure.step('Create contact if not exists'):
+        if app.contact.count() == 0:
+            app.contact.create(
+                Contact(first_name="created_first_name", middle_name="created_middle_name",
+                        last_name="created_last_name"))
     # создаем группу если групп нет
-    if len(db.get_group_list()) == 0:
-        app.group.create(
-            Group(name="created group", footer="created logo of group", header="created comment for group"))
+    with pytest.allure.step('Create group if not exists'):
+        if len(db.get_group_list()) == 0:
+            app.group.create(
+                Group(name="created group", footer="created logo of group", header="created comment for group"))
     # список групп из БД
-    old_groups = db.get_group_list()
+    with pytest.allure.step('Get group list from DB'):
+        old_groups = db.get_group_list()
     # берем рандомную группу
-    group = random.choice(old_groups)
-    group_id = group.id
+    with pytest.allure.step('Get random group'):
+        group = random.choice(old_groups)
+        group_id = group.id
     # берем контакты не входящие в эту группу
-    contacts_not_in_group = db_orm.get_contacts_not_in_group(group)
-    contact = random.choice(contacts_not_in_group)
-    contact_id = contact.id
+    with pytest.allure.step('Get contact not in group %s' % group):
+        contacts_not_in_group = db_orm.get_contacts_not_in_group(group)
+        contact = random.choice(contacts_not_in_group)
+        contact_id = contact.id
     # добавляем контакт в группу
-    app.contact.add_to_group_by_id(contact_id, group_id)
-    is_contact_in_group = False
+    with pytest.allure.step('Add contact in group'):
+        app.contact.add_to_group_by_id(contact_id, group_id)
+        is_contact_in_group = False
     # проверяем что в контакт добавился в группу
-    contacts_in_group = db_orm.get_contacts_in_group(group)
-    for item in range(len(contacts_in_group)):
-        if contacts_in_group[item].id == contact_id:
-            is_contact_in_group = True
-
-    assert is_contact_in_group == True
+    with pytest.allure.step('Verify contact %s was added to group %s' % (contact, group)):
+        contacts_in_group = db_orm.get_contacts_in_group(group)
+        for item in range(len(contacts_in_group)):
+            if contacts_in_group[item].id == contact_id:
+                is_contact_in_group = True
+        assert is_contact_in_group == True
--- test/test_delete_contact_from_group.py
+++ test/test_delete_contact_from_group.py
@@ -1,37 +1,49 @@
 import random
 
+import pytest
+
 from model.contact import Contact
 from model.group import Group
 
 
 def test_delete_contact_from_group(app, db, db_orm):
     # создаем новый контакт если контактов нет
-    if app.contact.count() == 0:
-        app.contact.create(
-            Contact(first_name="created_first_name", middle_name="created_middle_name", last_name="created_last_name"))
+    with pytest.allure.step('Create contact if not exists'):
+        if app.contact.count() == 0:
+            app.contact.create(
+                Contact(first_name="created_first_name", middle_name="created_middle_name",
+                        last_name="created_last_name"))
     # список контактов из БД
-    old_contacts = db.get_contact_list()
+    with pytest.allure.step('Get contact list from DB'):
+        old_contacts = db.get_contact_list()
     # берем рандомный контакт
-    contact = random.choice(old_contacts)
-    contact_id = contact.id
+    with pytest.allure.step('Get random contact'):
+        contact = random.choice(old_contacts)
+        contact_id = contact.id
     # создаем новую группу
-    if len(db.get_group_list()) == 0:
-        app.group.create(
-            Group(name="created group", footer="created logo of group", header="created comment for group"))
+    with pytest.allure.step('Create new group'):
+        if len(db.get_group_list()) == 0:
+            app.group.create(
+                Group(name="created group", footer="created logo of group", header="created comment for group"))
     # список групп из БД
-    old_groups = db.get_group_list()
+    with pytest.allure.step('Get group list from DB'):
+        old_groups = db.get_group_list()
     # берем рандомную группу
-    group = random.choice(old_groups)
-    group_id = group.id
+    with pytest.allure.step('Get random group'):
+        group = random.choice(old_groups)
+        group_id = group.id
     # добавляем контакт в группу
-    app.contact.add_to_group_by_id(contact_id, group_id)
+    with pytest.allure.step('Add contact %s to group with id %s' % (contact, group_id)):
+        app.contact.add_to_group_by_id(contact_id, group_id)
     # удаляем контакт из группы
-    app.contact.remove_from_group_by_id(contact_id)
-    is_contact_in_group = False
-    # проверяем что в контакт удалился из группы
-    contacts_not_in_group = db_orm.get_contacts_not_in_group(group)
-    for item in range(len(contacts_not_in_group)):
-        if contacts_not_in_group[item].id == contact_id:
-            is_contact_in_group = True
+    with pytest.allure.step('Delete contact from group'):
+        app.contact.remove_from_group_by_id(contact_id)
+    with pytest.allure.step('Verify contact was removed from group'):
+        is_contact_in_group = False
+        # проверяем что в контакт удалился из группы
+        contacts_not_in_group = db_orm.get_contacts_not_in_group(group)
+        for item in range(len(contacts_not_in_group)):
+            if contacts_not_in_group[item].id == contact_id:
+                is_contact_in_group = True
 
-    assert is_contact_in_group == True
+        assert is_contact_in_group == True
