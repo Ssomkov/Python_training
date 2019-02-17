cd5e8edf44ce92ab1036e5dab467faa5a3ec9cd0
ssomkov <ssomkov@gmail.com>
2019-02-17 14:19:36 +0400

    add gherkin delete contact

--- bdd/contact_steps.py
+++ bdd/contact_steps.py
@@ -1,7 +1,7 @@
 from pytest_bdd import when, then, given
 
 from model.contact import Contact
-
+import random
 
 @given('a contact list')
 def contact_list(db):
@@ -24,3 +24,32 @@ def verify_contact_added(db, contact_list, new_contact):
     new_contacts = db.get_contact_list()
     old_contacts.append(new_contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+@given('a non-empty contact list')
+def non_empty_contact_list(db, app):
+    if len(db.get_contact_list()) == 0:
+        app.contact.create(Contact(first_name="empty_name", middle_name="empty_name", last_name="empty_name"))
+    return db.get_contact_list()
+
+
+@given('a random contact from list')
+def random_contact(non_empty_contact_list):
+    return random.choice(non_empty_contact_list)
+
+
+@when('I delete a contact from list')
+def delete_contact(app, random_contact):
+    app.contact.delete_by_id(random_contact.id)
+
+
+@then('the new contact list is equal to the old list without deleted contact')
+def verify_contact_deleted(db, non_empty_contact_list, random_contact, app, check_ui):
+    old_contacts = non_empty_contact_list
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts) + 1
+    old_contacts.remove(random_contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
+                                                                     key=Contact.id_or_max)
--- bdd/contacts.feature
+++ bdd/contacts.feature
@@ -8,3 +8,9 @@ Examples:
 | first_name | middle_name | last_name |
 | first_name1 | middle_name1 | last_name1 |
 | first_name2 | middle_name12 | last_name2 |
+
+Scenario: Delete contact
+Given a non-empty contact list
+Given a random contact from list
+When I delete a contact from list
+Then the new contact list is equal to the old list without deleted contact
